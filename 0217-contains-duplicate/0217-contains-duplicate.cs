c85e702d21737eb0fc9185d2e3cc0f6dc86f75ad
Vaibhavi <vaibhaviusutar35@gmail.com>
2026-07-23 12:08:19 +0530

    Time: 13 ms (76.18%), Space: 66.1 MB (78.45%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,18 +1,6 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        bool result=false;
-        HashSet<int> map = new HashSet<int>();
-        foreach(var num in nums)
-        {
-            if(!map.Contains(num))
-            {
-                map.Add(num);
-            }
-            else
-            {
-                result = true;
-            }
-        }
-        return result;
+        HashSet<int> set = new HashSet<int>(nums);
+        return set.Count != nums.Length;
     }
 }
